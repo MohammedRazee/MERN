70655c0723e759cb4dd4d84882df3bac377bcece
razee <razeeismail15@gmail.com>
2026-08-11 09:27:07 +0530

    first update

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("First Node program")
+import express from "express"
+import path from "path"
+
+const app = express()
+const port = 3000
+
+const __dirname = import.meta.dirname;
+const templateDir = path.join(__dirname, "view")
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(templateDir, "index.html"))
+})
+
+app.listen(port, (res, req)=> {
+    console.log(`Listening on port http://localhost:${port}`)
+})
